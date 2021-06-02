95d55882882e68e45cd4ec4ba9636338c874330c
delta-kimigatame <sankaku.kuro@gmail.com>
2021-06-02 22:33:04 +0900

    envフォルダを管理対象から除外

--- Psd2Png/Psd2Png.py
+++ Psd2Png/Psd2Png.py
@@ -3,22 +3,101 @@ import shutil
 import os
 import os.path
 import re
+import logging
 from tkinter import messagebox
 from PIL import Image
 from psd_tools import PSDImage
 
+
+class Psd2Png:
+    __layerNameSanitizeReg=re.compile('[\\/:*?"<>|]+')
+    __defaultLayerName="layer"
+    __paddingChar="_"
+
+    def __init__(self,psdPath,outputDir=""):
+        self.__layerCount=0
+        self.__outputPaths=[]
+        self.__IsInputPathAsFile(psdPath)
+        self.__CheckFileFormat(psdPath)
+        self.__CheckOutputDir(psdPath,outputDir)
+
+
+    def __IsInputPathAsFile(self,psdPath):
+        if not os.path.isfile(psdPath):
+            logging.error ("指定されたファイルが見つかりません\n"+psdPath)
+            raise FileNotFoundError("指定されたファイルが見つかりません\n"+psdPath)
+        logging.info ("OK:"+psdPath)
+
+    def __CheckFileFormat(self,psdPath):
+        logging.info ("ファイルがpsdか確認")
+        try:
+            self.__psd=PSDImage.open(psdPath)
+        except:
+            logging.error ("指定されたファイルはpsdではありません\n"+psdPath)
+            raise OSError(400,"指定されたファイルはpsdではありません",psdPath)
+        logging.info ("OK")
+
+    def __CheckOutputDir(self,psdPath,outputDir):
+        logging.info ("保存先フォルダ確認")
+        if outputDir=="":
+            outputDir=psdPath.split(".")[0]
+            logging.debug("outputDirが指定されていないため、"+outputDir+"としました")
+
+        if os.path.isdir(outputDir):
+            logging.info(outputDir +"はすでに存在します")
+            updateAlert=messagebox.askquestion("上書きの確認",outputDir +"はすでに存在しますが、上書きしますか?")
+            if updateAlert=="yes":
+                logging.info("既存のファイル削除")
+                shutil.rmtree(outputDir)
+                logging.info ("OK")
+            else:
+                logging.info("ファイルの上書きがキャンセルされました")
+                raise FileExistsError("ファイルの上書きがキャンセルされました")
+        logging.info ("出力フォルダの生成")
+        os.makedirs(outputDir)
+        self.__outputDir=outputDir
+        logging.info ("OK")
+
+    def OutputPng(self):
+        self.SetOutputPaths()
+        print(self.__outputPaths)
+        for layer in list(self.__psd.descendants()):
+            if layer.is_group():
+                continue
+            #applylayeroffset
+            #save
+
+    def SetOutputPaths(self):
+        for layer in list(self.__psd.descendants()):
+            if layer.is_group():
+                continue
+            parentDirs=self.__GetParentDirs(layer)
+            outputFileName=self.__LayerNameSanitize(layer)
+            self.__SetOutputPath(parentDirs,outputFileName)
+
+
+    def __GetParentDirs(self,layer):
+        parentDirs=[]
+        parent=layer.parent
+        while "layers.Group" in str(type(parent)):
+            parentDirs.append(self.__LayerNameSanitize(parent))
+            parent=parent.parent
+        return parentDirs
+
+    def __LayerNameSanitize(self,layer):
+        newName=re.sub(self.__layerNameSanitizeReg,"",layer.name)
+        if newName=="":
+            newName=self.__defaultLayerNasme+str(self.__layerCount)
+            self.__layerCount+=1
+        return newName
+
+    def __SetOutputPath(self,parentDirs,outputFileName):
+        tempPath=os.path.join(newDirPath,*parentDirs,outputFileName)
+        while tempPath in self.__outputPaths:
+            tempPath+=self.__paddingChar
+        self.__outputPaths.append(tempPath)
+
 def main():
-    print ("ファイルパス確認")
-    if(len(sys.argv) <=1):
-        messagebox.showerror("ファイルをドラッグ&ドロップしてください","このソフトにpsdファイルをドラッグ&ドロップしてください")
-        return
-    psdPath=sys.argv[1]
-    psdDirPath,psdFileName=os.path.split(psdPath)
-    if not os.path.isfile(psdPath):
-        messagebox.showerror("ファイルが見つかりません","指定されたファイルが見つかりません")
-        return
-    
-    print ("OK")
     print ("ファイルがpsdか確認")
     try:
         psd=PSDImage.open(psdPath)
@@ -74,4 +153,12 @@ def main():
 
 
 if __name__=="__main__":
-    main()
+    #main()
+    logging.basicConfig(level=logging.INFO)
+    logging.info ("ファイルパス確認")
+    if(len(sys.argv) <=1):
+        messagebox.showerror("ファイルをドラッグ&ドロップしてください","このソフトにpsdファイルをドラッグ&ドロップしてください")
+    else:
+        psdPath=sys.argv[1]
+        P2P=Psd2Png(psdPath)
+        P2P.OutputPng()
