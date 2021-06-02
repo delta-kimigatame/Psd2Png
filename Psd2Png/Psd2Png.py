5e4bfccd7a9ebf1f7760120009dc11a07b3dce11
delta-kimigatame <sankaku.kuro@gmail.com>
2021-06-02 23:17:03 +0900

    実装のクラス化

--- Psd2Png/Psd2Png.py
+++ Psd2Png/Psd2Png.py
@@ -16,146 +16,123 @@ class Psd2Png:
 
     def __init__(self,psdPath,outputDir=""):
         self.__layerCount=0
+        self.__layers=[]
         self.__outputPaths=[]
         self.__IsInputPathAsFile(psdPath)
         self.__CheckFileFormat(psdPath)
         self.__CheckOutputDir(psdPath,outputDir)
+        self.__CheckLength()
 
 
     def __IsInputPathAsFile(self,psdPath):
         if not os.path.isfile(psdPath):
             logging.error ("指定されたファイルが見つかりません\n"+psdPath)
             raise FileNotFoundError("指定されたファイルが見つかりません\n"+psdPath)
-        logging.info ("OK:"+psdPath)
+        logging.debug ("OK:"+psdPath)
 
     def __CheckFileFormat(self,psdPath):
-        logging.info ("ファイルがpsdか確認")
+        logging.debug ("ファイルがpsdか確認")
         try:
             self.__psd=PSDImage.open(psdPath)
         except:
             logging.error ("指定されたファイルはpsdではありません\n"+psdPath)
             raise OSError(400,"指定されたファイルはpsdではありません",psdPath)
-        logging.info ("OK")
+        logging.debug ("ファイルがpsdか確認OK")
 
     def __CheckOutputDir(self,psdPath,outputDir):
-        logging.info ("保存先フォルダ確認")
+        logging.debug ("保存先フォルダ確認")
         if outputDir=="":
             outputDir=psdPath.split(".")[0]
-            logging.debug("outputDirが指定されていないため、"+outputDir+"としました")
+            logging.info("outputDirが指定されていないため、"+outputDir+"としました")
 
         if os.path.isdir(outputDir):
-            logging.info(outputDir +"はすでに存在します")
+            logging.debug(outputDir +"はすでに存在します")
             updateAlert=messagebox.askquestion("上書きの確認",outputDir +"はすでに存在しますが、上書きしますか?")
             if updateAlert=="yes":
-                logging.info("既存のファイル削除")
+                logging.debug("既存のファイル削除")
                 shutil.rmtree(outputDir)
-                logging.info ("OK")
+                logging.debug ("既存のファイル削除OK")
             else:
-                logging.info("ファイルの上書きがキャンセルされました")
+                logging.debug("ファイルの上書きがキャンセルされました")
                 raise FileExistsError("ファイルの上書きがキャンセルされました")
-        logging.info ("出力フォルダの生成")
+        logging.debug ("出力フォルダの生成")
         os.makedirs(outputDir)
         self.__outputDir=outputDir
-        logging.info ("OK")
+        logging.debug ("出力フォルダの生成OK")
+
+    def __CheckLength(self):
+        logging.debug ("レイヤー数の取得")
+        for layer in list(self.__psd.descendants()):
+            if layer.is_group():
+                continue
+            self.__layers.append(layer)
+        logging.debug ("取得OK:"+str(len(self.__layers)))
+
 
     def OutputPng(self):
-        self.SetOutputPaths()
-        print(self.__outputPaths)
-        for layer in list(self.__psd.descendants()):
-            if layer.is_group():
-                continue
-            #applylayeroffset
-            #save
+        logging.debug ("pngファイルの書き出し")
+        logging.debug ("出力リストの確認")
+        if len(self.__layers)!=len(self.__outputPaths):
+            logging.info ("レイヤー数と出力リストが一致しないため再取得します。")
+            self.SetOutputPaths()
+        for layer,output in zip(self.__layers,self.__outputPaths):
+            outputImage=self.__ApplyLayerOffset(layer)
+            outputDir=os.path.split(output)[0]
+            if not os.path.isdir(outputDir):
+                logging.debug (outputDir+"が存在しないため生成")
+                try:
+                    os.makedirs(outputDir)
+                except:
+                    logging.error("フォルダの生成に失敗しました:"+outputDir)
+            try:
+                outputImage.save(output+".png")
+                logging.info("レイヤー名："+layer.name+"を"+output+".pngに保存しました")
+            except:
+                logging.error("レイヤー名："+layer.name+"("+output+".png)の保存に失敗しました")
 
     def SetOutputPaths(self):
-        for layer in list(self.__psd.descendants()):
-            if layer.is_group():
-                continue
+        logging.debug ("レイヤー名の抽出")
+        for layer in self.__layers:
             parentDirs=self.__GetParentDirs(layer)
             outputFileName=self.__LayerNameSanitize(layer)
             self.__SetOutputPath(parentDirs,outputFileName)
-
+        logging.debug ("レイヤー名の抽出OK")
 
     def __GetParentDirs(self,layer):
+        logging.debug (layer.name+"のレイヤーグループ確認")
         parentDirs=[]
         parent=layer.parent
         while "layers.Group" in str(type(parent)):
             parentDirs.append(self.__LayerNameSanitize(parent))
             parent=parent.parent
+        logging.debug (layer.name+"のレイヤーグループ取得完了"+str(parentDirs))
         return parentDirs
 
     def __LayerNameSanitize(self,layer):
+        logging.debug (layer.name+"のうち、ファイル名に使えない文字の除去")
         newName=re.sub(self.__layerNameSanitizeReg,"",layer.name)
         if newName=="":
-            newName=self.__defaultLayerNasme+str(self.__layerCount)
+            newName=self.__defaultLayerName+str(self.__layerCount)
             self.__layerCount+=1
+            logging.debug (layer.name+"が0文字になったため、"+newName+"としました")
         return newName
 
     def __SetOutputPath(self,parentDirs,outputFileName):
-        tempPath=os.path.join(newDirPath,*parentDirs,outputFileName)
+        tempPath=os.path.join(self.__outputDir,*parentDirs,outputFileName)
         while tempPath in self.__outputPaths:
             tempPath+=self.__paddingChar
+        logging.debug ("書き出しファイル名リストに"+outputFileName+"を"+tempPath+"としてセット")
         self.__outputPaths.append(tempPath)
 
-def main():
-    print ("ファイルがpsdか確認")
-    try:
-        psd=PSDImage.open(psdPath)
-    except:
-        messagebox.showerror("フォーマットエラー","指定したファイルはpsdではありません")
-        return
-    print ("OK")
-
-    
-    print ("保存先フォルダ確認")
-    makeDirName=psdFileName.split(".")[0]
-    newDirPath=os.path.join(psdDirPath,makeDirName)
-    if os.path.isdir(newDirPath):
-        updateAlert=messagebox.askquestion("上書きの確認",makeDirName +"はすでに存在しますが、上書きしますか?")
-        if updateAlert=="yes":
-            shutil.rmtree(newDirPath)
-        else:
-            return
-    os.mkdir(newDirPath)
-    
-    print ("OK")
-    badFileNameReg=re.compile('[\\/:*?"<>|]+')
-    count=0
-    for layer in list(psd.descendants()):
-        if not layer.is_group():
-            parentDirs=[]
-            newFile=re.sub(badFileNameReg,"",layer.name)
-            parent=layer.parent
-            while "layers.Group" in str(type(parent)):
-                parentDirs.append(parent.name)
-                parent=parent.parent
-            print (parentDirs)
-            if len(parentDirs)!=0 and not os.path.isdir(os.path.join(newDirPath,*parentDirs)):
-                os.makedirs(os.path.join(newDirPath,*parentDirs))
-
-            if newFile=="":
-                newFile="layer"+str(count)
-            image=layer.topil()
-            bg=Image.new("RGBA",[psd.width,psd.height],(0,0,0,0))
-            bg.paste(image,layer.offset)
-
-            if os.path.isfile(os.path.join(newDirPath,*parentDirs,newFile+".png")):
-                tempFile=newFile+"_"+str(count)
-                while os.path.isfile(os.path.join(newDirPath,*parentDirs,tempFile+".png")):
-                    tempFile+="_"
-                newFile=tempFile
-            try:
-                bg.save(os.path.join(newDirPath,*parentDirs,newFile+".png"))
-                print("レイヤー名："+layer.name+"を"+os.path.join(newDirPath,*parentDirs,newFile+".png")+"に保存しました")
-            except:
-                print("レイヤー名："+layer.name+"の保存に失敗しました")
-            count+=1
-
+    def __ApplyLayerOffset(self,layer):
+        image=layer.topil()
+        bgi=Image.new("RGBA",[self.__psd.width,self.__psd.height],(0,0,0,0))
+        bgi.paste(image,layer.offset)
+        return bgi
 
 if __name__=="__main__":
-    #main()
-    logging.basicConfig(level=logging.INFO)
-    logging.info ("ファイルパス確認")
+    logging.basicConfig(level=logging.DEBUG)
+    logging.debug ("ファイルパス確認")
     if(len(sys.argv) <=1):
         messagebox.showerror("ファイルをドラッグ&ドロップしてください","このソフトにpsdファイルをドラッグ&ドロップしてください")
     else:
